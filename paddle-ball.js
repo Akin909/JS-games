e973c92eda6def99e70f66ab242fcc328b8c89e6
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-15 00:03:20 +0000

    add updated background with transitioning background, scores and reset

--- paddle-ball.js
+++ paddle-ball.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
@@ -17,14 +18,20 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+let score = 0;
+const showScore = document.querySelector("span.score")
+console.log(showScore)
+
 
 var bricks = [];
 for(c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
     for(r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
+
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -46,10 +53,12 @@ function keyUpHandler(e) {
     }
 }
 
+let ballColor = "#ba3714";
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = ballColor
     ctx.fill();
     ctx.closePath();
 }
@@ -63,24 +72,47 @@ function drawPaddle() {
 function drawBricks() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
+          if(bricks[c][r].status === 1){
             var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
             var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
             bricks[c][r].x = brickX;
             bricks[c][r].y = brickY;
             ctx.beginPath();
             ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
+            ctx.fillStyle = "#dded5f";
             ctx.fill();
             ctx.closePath();
+          }
         }
     }
 }
+
+function collisionDetection(){
+  for(var c = 0; c<brickColumnCount;c++){
+    for(var r = 0; r<brickRowCount; r++){
+      var brickPosition = bricks[c][r]
+      if(brickPosition.status === 1){
+      //x variable here is the x position of the ball and y is its y position
+        if(x > brickPosition.x && x <brickPosition.x+brickWidth && y > brickPosition.y && y < brickPosition.y + brickHeight){
+          dy = -dy;
+          brickPosition.status = 0;
+          ballColor = "#604a11"
+          score +=1;
+          showScore.innerHTML = score;
+          if(score>=8){showScore.innerHTML+=` <p class="score-message">Good Job!!</p>`}
+        }
+      }
+    }
+  }
+}
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection()
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -95,8 +127,9 @@ function draw() {
 			 }
         }
         else {
-            alert("GAME OVER");
-            document.location.reload();
+            ctx.font = '3em Nunito Sans';
+            ctx.fillText("Game Over",125,200)
+            clearInterval(myInterval)
         }
     }
 
@@ -110,5 +143,10 @@ function draw() {
     x += dx;
     y += dy;
 }
-
-setInterval(draw, 10);
+window.addEventListener("keydown",(event)=>{
+  if(event.keyCode === 32){
+    //restart the Game
+     document.location.reload();
+  }
+})
+let myInterval = setInterval(draw, 10);
